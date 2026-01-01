95dc12bbceddf03accbe750cf0a510a71051c2b5
Marvel123g <marvellousolorunwa23@gmail.com>
2026-01-01 12:28:55 +0100

    Added the nav having a text of forge to all the pages along side with the sidebar on the left

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,19 @@ import Form from "./components/Form";
 import Analytics from "./components/Analytics";
 import Timeline from "./components/Timeline";
 import Narration from "./components/Narration";
+import ScrollToTop from "./components/ScrollToTop";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/form" element={<Form />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/timeline" element={<Timeline />} />
-      <Route path="/narration" element={<Narration />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/narration" element={<Narration />} />
+      </Routes>
+    </>
   );
 }
 
