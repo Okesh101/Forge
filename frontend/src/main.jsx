85bd5acc45d9af67e54384aafefce0dbc2b6e335
Marvel123g <marvellousolorunwa23@gmail.com>
2026-01-05 20:13:51 +0100

    Saving the session id and adding it to header when POSTing valuse

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./scss/main.scss";
+import { SessionProvider } from "./contextApi/SessionContext.jsx";
 // import "./add.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SessionProvider>
+        <App />
+      </SessionProvider>
     </BrowserRouter>
   </StrictMode>
 );
